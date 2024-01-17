521c764a41378e1097d8a7e1e647cb3dc37bdfa5
Akshat Aggarwal <akshataggarwal6004@gmail.com>
2024-01-17 14:21:57 +0000

    Completed Data Types

---
+++ 01_JS_Basics/01_variables.js
@@ -0,0 +1,13 @@
+// const => Constants cannot be changed afterwards
+// let => A simple variable
+
+const accountId = 14455236
+let accountEmail = 'ex@mail.com'
+var accountPass = '14455236@#'  // Not used any more and should not be used isue with scope with var
+accountCity = "Rajkot"  // Not a good practice
+
+// accountId = 5
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPass,accountCity])
---
+++ 01_JS_Basics/02_Data_Type.js
@@ -0,0 +1,29 @@
+// Primitive Data Types 7 types
+/*
+1. String
+2. Number
+3. Boolean
+4. Null
+5. Undefined
+6. Symbol
+7. BigInt
+*/
+
+let name = "Akshat"  // String Type
+let age = 22         //Number can also use BigInt data type for very very large values
+let isSingle = true  // Boolean true or false
+// null => type and standalone value 
+// undefined=> hamne kuch nahi diya input 
+// Symbol => Mainly used for uniqueness
+
+// Non Primitive
+/*
+1. Objects
+2. Arrays
+3. Functions
+*/
+
+
+// typeof => used to check type of a variable
+
+console.table([typeof String, typeof Number, typeof null, typeof Symbol, typeof undefined, typeof Object, typeof Array, typeof function(){}])
